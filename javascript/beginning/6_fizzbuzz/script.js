d16d256dcaa9619ea73c7d454abc2dc2d520217c
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2018-09-14 16:17:24 +0900

    [add]FizzBuzzを、for、関数等を使って表示
    ul等を使ってhtmlに表示している

---
+++ javascript/beginning/6_fizzbuzz/script.js
@@ -0,0 +1,26 @@
+var canDivide = function(devidedNumber, devidingNumber) {
+    return (devidedNumber % devidingNumber) === 0; 
+}
+
+var outputFizzBuzz = function(number) {
+    if (canDivide(number, 15)) {
+        return "FizzBuzz!";
+    }
+    if (canDivide(number, 5)) {
+        return "Buzz!";
+    }
+    if (canDivide(number, 3)) {
+        return "Fizz!";
+    }
+    return number;
+}
+
+var outputList = function(number) {
+    for (i = 1; i <= number; i++) {
+        var li = document.createElement('li');
+        li.textContent = outputFizzBuzz(i);
+        document.getElementById('fizzbuzz').appendChild(li);
+    }
+}
+
+outputList(93);
