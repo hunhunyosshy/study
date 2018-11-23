181037a1516508daf323e9bf5a4340f6dbfab96e
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2018-11-23 17:18:37 +0900

    数字リテラルについて学ぶ
    0x が16進数、0o が8進数、0b が2進数

---
+++ javascript/beginning/9_literal/script.js
@@ -0,0 +1,13 @@
+//10進数
+let number10 = 100;
+//16進数
+let number16 = 0x64;
+//8進数
+let number8 = 0o144;
+//2進数
+let number2 = 0b1100100;
+
+console.log(number10);
+console.log(number16);
+console.log(number8);
+console.log(number2);
