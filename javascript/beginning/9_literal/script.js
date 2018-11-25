f6866d06eeb2a28175686fec3e3e8da4adfc0755
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2018-11-25 14:51:08 +0900

    jsのデータ型について知ったことを書く

--- javascript/beginning/9_literal/script.js
+++ javascript/beginning/9_literal/script.js
@@ -7,7 +7,16 @@ let number8 = 0o144;
 //2進数
 let number2 = 0b1100100;
 
+let name = 'JavaScript';
+let string = `こんにちは、${name}
+ES2015 でいろいろ変わったんですね`;
+
+//JavaScript では、
+//「変数の値が定義されてない状態」がundefined
+//「該当する値が見つからなかった、空の状態」がnull らしい
+
 console.log(number10);
 console.log(number16);
 console.log(number8);
 console.log(number2);
+console.log(string);
