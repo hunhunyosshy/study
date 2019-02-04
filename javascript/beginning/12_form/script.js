2177d49ffe2bcb949c5d9def8de0188518ec6f54
Kazuki Yoshikawa <yoshikawa.kazuki14@gmail.com>
2019-02-04 22:25:22 +0900

    [add]ファイル追加

--- javascript/beginning/12_form/script.js
+++ javascript/beginning/12_form/script.js
@@ -1,5 +1,8 @@
 //ブラウザに動きがあったことをイベントと呼ぶ onsubmit もその1つ
-//この場合、クロージャを使っているのでもちろんアロー関数も使える
+//この場合、クロージャを使っているのでもちろん以下のようにアロー関数も使える
 document.getElementById('form').onsubmit = () => {
-  console.log('クリックされました');
+  const word = document.getElementById('form').word.value;
+  //console.log('クリックされました');
+  document.getElementById('output').textContent = '「' + word + '」';
+  return false;
 }
